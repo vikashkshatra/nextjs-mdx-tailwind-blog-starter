69f0eb8bb7d525f4e2c56e185a6c5d074d234f2d
vikashkshatra <kshatravikash@gmail.com>
2022-05-12 15:00:01 +0530

    starter

---
+++ components/Layout.js
@@ -0,0 +1,12 @@
+
+
+export default function Layout({ children }) {
+    return (
+      <>
+        <div className="wrapper max-w-xl p-6 my-0 mx-auto">
+          {children}
+        </div>
+      </>
+    )
+  }
+  
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    'pages/**/*.{js,jsx,md,mdx}',
+    'components/**/*.{js,jsx,md,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
